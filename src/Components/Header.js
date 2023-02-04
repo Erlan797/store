45a04cb7b671e1865ff66003ed1c8bb2bdc53764
Erlan <erlanamanbekov797@gmail.com>
2023-02-04 12:20:28 +0300

    Begin to create the anime store

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+        <div>
+            <span className='logo'>Anime Magazine</span>
+        </div>
+        <div className='presentatino'></div>
+    </header>
+  )
+}
