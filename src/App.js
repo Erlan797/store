d495dc941e4d3dfcb8d2a0fe87d80b62da6792e0
Erlan <erlanamanbekov797@gmail.com>
2023-02-06 17:38:44 +0300

    Make a lot things

--- src/App.js
+++ src/App.js
@@ -1,7 +1,79 @@
 import React from 'react'
-
-export default function App() {
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Items from './Components/Items'
+import './index.css'
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      orders:[],
+      items: [
+        {
+          id: 1,
+          title: 'Kakashi cup',
+          img: 'kakashi.jpg',
+          desc: "It is a cup of Kakshi from Naruto",
+          category: 'cup',
+          price: "49.90"
+        },
+        {
+          id: 2,
+          title: 'Tangiro',
+          img: 'tangiro.jpg',
+          desc: "It is a cup from The Demon Slayer",
+          category: 'cup',
+          price: "25"
+        },
+        {
+          id: 3,
+          title: 'Naruto',
+          img: 'Naruto.jpg',
+          desc: "It is a cup from Naruto",
+          category: 'cup',
+          price: "44.50"
+        },
+        {
+          id: 4,
+          title: 'Akatsuki',
+          img: 'Akatsuki.jpg',
+          desc: "It is a cup from Naruto",
+          category: 'cup',
+          price: "36.66"
+        },
+        {
+          id: 5,
+          title: 'Itachi',
+          img: 'Itachi.jpg',
+          desc: "It is a cup from Naruto",
+          category: 'cup',
+          price: "28"
+        },
+      ]
+    }
+    this.addOrder = this.addOrder.bind(this);
+    this.deleteOrder=this.deleteOrder.bind(this);
+  }
+  render(){
   return (
-    <div>App</div>
+    <div className='wrapper'>
+         <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
+         <Items items={this.state.items} onAdd={this.addOrder}/>
+         <Footer />
+    </div>
   )
 }
+deleteOrder(id){
+    this.setState({orders: this.state.orders.filter(el => el.id !== id)})
+}
+addOrder(item){
+  let isInArray = false;
+  this.state.orders.forEach(el =>{
+    if(el.id === item.id)
+    isInArray = true
+})
+  if(!isInArray)
+this.setState({orders: [...this.state.orders, item]})
+}
+}
+export default App;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaShoppingCart } from "react-icons/fa";
+import Order from './Order';
+const showOrders = (props) => {
+  let sum = 0;
+  props.orders.forEach(element => sum += Number.parseFloat(element.price));
+  return (<div>
+    {props.orders.map(el => (
+      <Order onDelete={props.onDelete} key={el.id} item={el} />
+    ))}
+    <p className='sum'>Sum: {new Intl.NumberFormat().format(sum)}$</p>
+  </div>)}
+  const showNothing = (props) => {
+    return(<div className='empty'>
+       <h2>No goods</h2>
+    </div>)
+  }
 
-export default function Header() {
+export default function Header(props) {
+  let [carOpen, setcarOpen] = useState(false);
+
   return (
     <header>
-        <div>
-            <span className='logo'>Anime Magazine</span>
-        </div>
-        <div className='presentatino'></div>
+      <div>
+        <span className='logo'>Anime Store</span>
+        <ul className='nav'>
+          <li>Goods</li>
+          <li>Profile</li>
+          <li>Contact</li>
+        </ul>
+        <FaShoppingCart onClick={() => setcarOpen(carOpen = !carOpen)} className={`shop-card ${carOpen && 'active'}`} />
+        {carOpen && (
+          <div className='shop-cart '>
+            {props.orders.length > 0 ? showOrders(props) : showNothing()}
+
+          </div>
+        )}
+      </div>
+      <div className='presentation'></div>
     </header>
   )
 }
